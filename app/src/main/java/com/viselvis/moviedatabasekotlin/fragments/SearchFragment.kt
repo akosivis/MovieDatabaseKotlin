5f5b128a1c6ce57d4877ee5b5544cd1fb0a1f610
theviselvis <visntr@gmail.com>
2021-03-03 10:40:32 +0800

    Initial implementation of Room DB
    - Creation of AppDatabase.kt, ContactDao.kt, Contact.kt
    - Removal of unnecessary codes in ContactFragment.kt, SearchFragment.kt
    - Minor modifications on strings.xml and main_menu.xml

--- app/src/main/java/com/viselvis/moviedatabasekotlin/fragments/SearchFragment.kt
+++ app/src/main/java/com/viselvis/moviedatabasekotlin/fragments/SearchFragment.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.viselvis.moviedatabasekotlin.`interface`.ApiService
 import com.viselvis.moviedatabasekotlin.adapters.MovieAdapter
 import com.viselvis.moviedatabasekotlin.databinding.FragmentSearchBinding
